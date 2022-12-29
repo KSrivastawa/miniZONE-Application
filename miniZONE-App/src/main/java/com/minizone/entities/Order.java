ed1d6d43753c5b6f8ea3f1778502bf2d51b861c5
sakshi-i <sakshidwivedi171@gmail.com>
2022-12-30 00:50:40 +0530

    updated

--- miniZONE-App/src/main/java/com/minizone/entities/Order.java
+++ miniZONE-App/src/main/java/com/minizone/entities/Order.java
@@ -1,5 +1,61 @@
 package com.minizone.entities;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.Pattern;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Entity
+@Setter
+@Getter
+@Table(name = "orders")
 public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "order_id")
+    private Long orderId;
+
+    @JsonIgnore
+    @Column(name = "date_time")
+    private LocalDateTime dateTime = LocalDateTime.now();
+
+    @JsonIgnore
+    @Column(name = "order_status")
+    private String orderStatus = "Shipping...";
+
+    @Pattern(regexp = "UPI|CashOnDelivery|Card")
+    private String paymentMode;
+
+
+    @JsonIgnore
+    @OneToOne
+    private Address address;
+
+
+    @JsonIgnore
+    @OneToOne
+    private User user;
+
+
+    @JsonIgnore
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<Product> productList = new ArrayList<>();
 
 }
--- miniZONE-App/src/main/java/com/minizone/servicesImplementation/OrderServiceImplementation.java
+++ miniZONE-App/src/main/java/com/minizone/servicesImplementation/OrderServiceImplementation.java
@@ -1,5 +1,91 @@
 package com.minizone.servicesImplementation;
 
+import com.minizone.entities.*;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 public class OrderServiceImplementation {
+
+    @Service
+public class OrderServiceImplementation implements OrderService {
+
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private AddressRepository addressRepository;
+
+
+    @Override
+    public String addOrder(Order order, Long addressId, Long userID) throws OrderException {
+
+        String message = "Not Ordered!";
+
+        User user = userRepository.findById(userID).orElseThrow(()-> new OrderException("User not found!"));
+
+        Address address = addressRepository.findById(addressId).orElseThrow(()-> new OrderException("Address not found for this respective user!"));
+
+        if(user != null) {
+
+            if(user.getAddress().get(0) != null) {
+
+                order.setUser(user);
+                order.setAddress(address);
+                orderRepository.save(order);
+
+                message = "Order Saved Successfully!";
+
+            }else {
+                throw new OrderException("Address not found for this respective user!");
+            }
+
+        }
+
+        return message;
+
+    }
+    @Override
+    public String deleteOrderById(Long orderID) throws OrderException {
+
+        String message = "Not Deleted!";
+
+        Order order = orderRepository.findById(orderID).orElseThrow(()-> new OrderException("Order Not Found!"));
+
+        if(order != null) {
+            orderRepository.delete(order);
+            message = "Order Deleted!";
+        }
+
+        return message;
+
+    }
+
+    @Override
+    public Order getOrderById(Long orderID) throws OrderException {
+
+        Order order = orderRepository.findById(orderID).orElseThrow(()-> new OrderException("Order Not Found!"));
+
+        return order;
+    }
+
+    @Override
+    public List<Order> getAllOrders() throws OrderException {
+
+        List<Order> orders = orderRepository.findAll();
+
+        if(orders.isEmpty()) throw new OrderException("Order List is empty!");
+
+        return orders;
+    }
+
+
+
+}
 
 }
